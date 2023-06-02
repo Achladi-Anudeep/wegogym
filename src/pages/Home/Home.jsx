6207589a10701cf04f1f31a4695ba725104424c8
Achladi-Anudeep <achladi.sai.anudeep@gmail.com>
2023-06-02 10:59:13 +0530

    added join section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,6 +13,40 @@ const Home = () => {
           alt="gym-img"
         ></img>
       </div>
+      <div className="join-sec">
+        <p>
+          EXPERIENCE THE FITNESS WAVE
+          <br />
+          AT MUMBAI'S FRIENDLIEST GYM!
+        </p>
+        <h1>JOIN WEGOGYM TODAY</h1>
+        <form>
+          <div className="form-input-sec">
+            <input type="text" placeholder="Name" />
+            <input type="emain" placeholder="Email" />
+            <input type="text" placeholder="Mobile" />
+            <select name="class">
+              <option value="" disabled selected hidden>
+                Choose One
+              </option>
+              <option value="">Free 1-Day Pass</option>
+              <option value="">Complimentary Personal Training</option>
+              <option value="">Complimentary Zumba Class</option>
+              <option value="">
+                Complimentary Spin Class/Indoor Cycling Class
+              </option>
+              <option value="">Complimentary Power Yoga Class</option>
+              <option value="">Complimentary Kickboxing Class</option>
+            </select>
+          </div>
+          <button type="submit">SEND</button>
+        </form>
+        <p>
+          #19YEARS OF SERVICE TO THE FITNESS & WELNESS COMMUNITY.
+          <br />
+          #HEREFORYOU #HERETOSTAY
+        </p>
+      </div>
     </div>
   );
 };
